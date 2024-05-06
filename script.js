f528e17ab1ad81469f09764109c819f070398176
vishalkj2002 <imrvishal28@gmail.com>
2024-05-06 23:36:32 +0530

    added

---
+++ script.js
@@ -0,0 +1,40 @@
+// var i = 0;
+// var txt = 'TEDxSGVS Youth is Happening on 29 June 2024.';
+// var speed = 50;
+
+// function typeWriter() {
+//     if(i < txt.length) {
+//         document.getElementById("demo").innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typeWriter, speed);
+//     }
+// }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
